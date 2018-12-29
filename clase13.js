739c03e8aaf89d508dd75d9f9b0b643914eb5758
Jhurgen Maraza <jhurgenmrz99@gmail.com>
2018-12-29 11:41:00 -0300

    Class 17 While

--- clase13.js
+++ clase13.js
@@ -2,5 +2,37 @@ var jhurgen = {
     nombre: 'Jhurgen',
     apellido: 'Maraza',
     edad: 19,
-    pedo: 70
-},
+    peso: 70
+}
+
+
+console.log(`Al inicio del año ${jhurgen.nombre} pesa ${jhurgen.peso}kg `)
+
+const INCREMENTO_DE_PESO= 0.3
+const DIAS_DEL_ANO=365
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_DE_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_DE_PESO
+const comeMucho = () => Math.random() < 0.3
+const realizaDeporte = () => Math.random() < 0.4
+
+const META = jhurgen.peso - 3
+var dias = 0
+
+while(jhurgen.peso > META){
+    
+    if(comeMucho()){
+    //Aumentar de Peso
+    
+    aumentarDePeso(jhurgen)
+    }
+    if(realizaDeporte() ){
+    //Adelgazar
+    adelgazar(jhurgen)
+    }
+    dias+=1
+}
+
+
+// console.log(`Al final del año ${jhurgen.nombre} pesa ${jhurgen.peso.toFixed(1)}kg `)
+console.log(`Pasaron ${dias} dias hasta que ${jhurgen.nombre} adelgazara 3kg`)
