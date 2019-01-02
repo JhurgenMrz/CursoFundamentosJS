a901ff75cf2cca36ed79c7644e4c492605d765f9
Jhurgen Maraza <jhurgenmrz99@gmail.com>
2019-01-02 15:18:31 -0300

    Class POO JS class28

--- class21.js
+++ class21.js
@@ -1,24 +1,42 @@
-function Persona(nombre, apellido,altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura= altura
-}
+// function heredaDe(prototipoHijo,prototipoPadre){
+//     var fn = function (){}
+//     fn.prototype = prototipoPadre.prototype
+//     prototipoHijo.prototype =new fn
+//     prototipoHijo.prototype.constructor = prototipoHijo
+// }
 
-Persona.prototype.saludar = function (){
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-}
-
-Persona.prototype.SoyAlto = function(){
-    if(this.altura>=1.80){
-        console.log(`Soy Alto! Segun los estandares! `)
+class Persona{
+    constructor(nombre, apellido,altura){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura= altura
     }
-    else{
-        console.log(`Soy Mediano Segun los estandares!`)
+    saludar(){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+    }
+    SoyAlto(){
+        if(this.altura>=1.80){
+            console.log(`Soy Alto! Segun los estandares! `)
+        }
+        else{
+            console.log(`Soy Mediano Segun los estandares!`)
+        }
     }
 }
 
-var jhurgen = new Persona('Jhurgen','Maraza',1.65)
+class Desarrollador extends Persona {
+    constructor(nombre,apellido,altura){
+        super(nombre,apellido,altura)
+    }
+    saludar(){
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
 
-var erika = new Persona('Erika','Luna',1.80)
+    }
+}
 
-var arturo = new Persona('Arturo','Martinez',1.70)
+
+// var jhurgen = new Persona('Jhurgen','Maraza',1.65)
+
+// var erika = new Persona('Erika','Luna',1.80)
+
+// var arturo = new Persona('Arturo','Martinez',1.70)
