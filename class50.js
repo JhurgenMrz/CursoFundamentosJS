204d201d8cf8855e23ea6c0df014c10e6db50579
Jhurgen Maraza <jhurgenmrz99@gmail.com>
2019-01-03 22:16:58 -0300

    Platzi Game

--- class50.js
+++ class50.js
@@ -18,41 +18,18 @@ function onError(id){
     console.log(`Sucedió un error al obtener el personaje ${id}`)
 }
 
-var ids=[1,2,3,4,5,6,7]
+async function obtenerPersonajes(){
+    var ids=[1,2,3,4,5,6,7]
+    var promesas = ids.map(id => obtenerPersonaje(id))
+    try{
+        var personajes = await Promise.all(promesas)
+        console.log(personajes)
+    }catch(id){
+        onError(id)
+    }
+}
 
-var promesas = ids.map(id => obtenerPersonaje(id))
+obtenerPersonajes()
 
-Promise
-    .all(promesas)
-    .then(personaje => console.log(personaje))
-    .catch(onError)
 
-/*obtenerPersonaje(1)
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(2)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(3)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(4)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(5)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(6)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-        return obtenerPersonaje(7)
-    })
-    .then((data) => {
-        console.log(`El personaje es ${data.name}`)
-    })
-    .catch(onError)*/
+
